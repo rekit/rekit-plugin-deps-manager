d8bfc166fa6030ac4df6e430126fd8df5f23eae4
Nate Wang <supnate@gmail.com>
2019-04-30 02:01:15 +0800

    Use shouldUse instead of featureFiles.

--- core/index.js
+++ core/index.js
@@ -1,6 +1,8 @@
 // Export plugin here if it extends rekit-core or rekit-studio.
+const fs = require('fs');
 
 module.exports = {
   studio: require('./studio'),
   app: require('./app'),
+  shouldUse: () => fs.existsSync(rekit.core.paths.map('package.json'))
 };
